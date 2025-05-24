81c2c4ac8b681517d5e5236f58811c32b292ea34
IslamTamjid <towhidulislam24@protonmail.com>
2025-05-24 10:06:07 +0600

    Add primary mongoose query method demonstrations

--- MONGODB-BASICS/app.js
+++ MONGODB-BASICS/app.js
@@ -24,27 +24,56 @@ const User = mongoose.model('User', userSchema);
 
 async function runQueryExamples() {
     try{
-
-        //create a new document
-        // const newUser = await User.create({
-        //     name: "Kevin Doe",
-        //     email: "Kendoe@email.com",
-        //     age: 34,
-        //     isActive: true,
-        //     tags: ['developer', 'designer', 'manager'],
-        // });
-
-        const newUser = new User({
-          name: "Kevin Doe",
-          email: "Kendoe@email.com",
-          age: 34,
+      //create a new document
+      const newUser = await User.create({
+          name: "Updated User",
+          email: "updateduser@email.com",
+          age: 75,
           isActive: true,
-          tags: ["developer", "designer", "manager"],
-        });
+          tags: ['developer'],
+      });
 
-        await newUser.save()
+      console.log("New user created successfully", newUser);
 
-        console.log("New user created successfully", newUser);
+      // const allUsers = await User.find({});
+      // console.log(allUsers);
+
+      // const getUserofActiveFalse = await User.find({isActive: true})
+      // console.log(getUserofActiveFalse)
+
+      // const getJohnDoeUser = await User.findOne({name: 'John Doe'})
+      // console.log(getJohnDoeUser)
+
+      // const getLastCreatedUserByUserId = await User.findById(newUser._id)
+      // console.log("Latest User", getLastCreatedUserByUserId)
+
+      // const getUserById = await User.findById('6831343c63229f42aa5ff095')
+      // console.log(getUserById)
+
+      // const selectedFields = await User.find().select('name email -_id ')
+      // console.log(selectedFields);
+
+      // const limitedUsers = await User.find().limit(5).skip(1);
+      // console.log(limitedUsers);
+
+      // sort users in descending order
+      // const sortedUserDescending = await User.find().sort({ age: -1 });
+      // console.log(sortedUserDescending);
+
+      // sort users in ascending order
+      // const sortedUserAscending = await User.find().sort({ age: 1 });
+      // console.log(sortedUserAscending);
+
+      // const countDocumentsByActivity = await User.countDocuments({ isActive: true })
+      // console.log(countDocumentsByActivity);
+
+      // const deletedUser = await User.findByIdAndDelete(newUser._id);
+      // console.log("This user is deleted from the database",deletedUser);
+
+      const updatedUser = await User.findByIdAndUpdate(newUser._id, {
+        $set : {age: 100}, $push: {tags : 'updated'}
+      }, {new: true})
+      console.log('Updated user', updatedUser)
 
     } catch(e) {
         console.log("Error: ", e)
