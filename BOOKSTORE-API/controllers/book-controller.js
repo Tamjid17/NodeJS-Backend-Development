54568b75af58c4eca9b8fb415ce6dd4d1029f55d
IslamTamjid <towhidulislam24@protonmail.com>
2025-06-29 03:21:53 +0600

    add controller logic for add book route

--- BOOKSTORE-API/controllers/book-controller.js
+++ BOOKSTORE-API/controllers/book-controller.js
@@ -1,4 +1,4 @@
-
+const Book = require('../models/Book')
 
 const getAllBooks = async (req, res) => {
 
@@ -6,7 +6,22 @@ const getAllBooks = async (req, res) => {
 
 const getSingleBookById = async (req, res) => {};
 
-const addNewBook = async (req, res) => {};
+const addNewBook = async (req, res) => {
+    try {
+        const newBookFormData = req.body;
+        const newlyCreatedBook = await Book.create(newBookFormData)
+        if(newBookFormData) {
+            res.status(201).json({
+                success : true,
+                message : "Book added successflly",
+                data : newlyCreatedBook
+            });
+        }
+    }
+    catch(e) {
+        console.error("Error: ", e);
+    }
+};
 
 const updateBook = async (req, res) => {};
 
