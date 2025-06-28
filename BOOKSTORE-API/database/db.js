14a4d46a453f597f346e86b7ad301af296f798e6
IslamTamjid <towhidulislam24@protonmail.com>
2025-06-29 00:36:28 +0600

    add mongoose connection to project

---
+++ BOOKSTORE-API/database/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const connectToDB = async() => {
+    try {
+        await mongoose.connect(
+            `mongodb+srv://towhidulislam24:${process.env.DB_PASS}@meals.kceb8.mongodb.net/<dbname>?retryWrites=true&w=majority`
+        );
+        console.log("Database connected succssfully");
+    } catch(e) {
+        console.error("Database connection failed", e);
+        process.exit()
+    } finally {
+        console.log("Database connected successfully");
+    }
+}
+
+module.exports = connectToDB
