7cc9fb1949b2e39210cc2f61c90c0172752bdfe1
IslamTamjid <towhidulislam24@protonmail.com>
2025-06-30 12:11:52 +0600

    add admin routes to server configuration

--- NODEJS-AUTH/server.js
+++ NODEJS-AUTH/server.js
@@ -3,7 +3,8 @@ dotenv.config();
 const express = require('express');
 const connectToDB = require('./database/db');
 const authRoutes = require('./routes/auth-routes');
-const homeRoutes = require('./routes/home-routes');
+const homeRoutes = require("./routes/home-routes");
+const adminRoutes = require("./routes/admin-routes");
 
 connectToDB()
 
@@ -14,6 +15,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/home', homeRoutes);
+app.use('/api/admin', adminRoutes);
 app.listen(PORT, () => {
     console.log(`Server is now running on port ${PORT}`);
 })
