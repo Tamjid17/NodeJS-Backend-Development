af9699baa539789bf10ab7820ce8ccfb4ecb2ad4
IslamTamjid <towhidulislam24@protonmail.com>
2025-06-30 11:26:09 +0600

    add auth routes and home routes

--- NODEJS-AUTH/server.js
+++ NODEJS-AUTH/server.js
@@ -1,16 +1,19 @@
 const dotenv = require('dotenv');
 dotenv.config();
-
 const express = require('express');
-
 const connectToDB = require('./database/db');
+const authRoutes = require('./routes/auth-routes');
+const homeRoutes = require('./routes/home-routes');
+
 connectToDB()
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//middlewares
 app.use(express.json());
-
+app.use('/api/auth', authRoutes);
+app.use('/api/home', homeRoutes);
 app.listen(PORT, () => {
     console.log(`Server is now running on port ${PORT}`);
 })
