42a995c095694725975bb7743507dffb8193f903
IslamTamjid <towhidulislam24@protonmail.com>
2025-07-03 02:16:45 +0600

    integrate image route to server file

--- NODEJS-AUTH/server.js
+++ NODEJS-AUTH/server.js
@@ -5,6 +5,7 @@ const connectToDB = require('./database/db');
 const authRoutes = require('./routes/auth-routes');
 const homeRoutes = require("./routes/home-routes");
 const adminRoutes = require("./routes/admin-routes");
+const uploadImageRoutes = require('./routes/image-route');
 
 connectToDB()
 
@@ -16,6 +17,8 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/home', homeRoutes);
 app.use('/api/admin', adminRoutes);
+app.use('/api/image', uploadImageRoutes);
+
 app.listen(PORT, () => {
     console.log(`Server is now running on port ${PORT}`);
 })
