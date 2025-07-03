e3b7f52384859357bec1e56fa777c9ab9e467fbf
IslamTamjid <towhidulislam24@protonmail.com>
2025-07-03 22:38:39 +0600

    add change password route to auth routes

--- NODEJS-AUTH/routes/auth-routes.js
+++ NODEJS-AUTH/routes/auth-routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const {registerUser, loginUser} = require('../controllers/auth-controller')
+const { registerUser, loginUser, changePassword } = require('../controllers/auth-controller')
+const authMiddlware = require('../middleware/auth-middleware');
 const router = express.Router();
 
 // all routes are related to authentication & authorization
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-
+router.post("/change-password", authMiddlware, changePassword);
 module.exports = router;
