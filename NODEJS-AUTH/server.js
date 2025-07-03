08c7c0a1272437fb10ae27d6bb981e18badce8cd
IslamTamjid <towhidulislam24@protonmail.com>
2025-07-03 22:37:58 +0600

    refactor image route variable name

--- NODEJS-AUTH/server.js
+++ NODEJS-AUTH/server.js
@@ -5,7 +5,7 @@ const connectToDB = require('./database/db');
 const authRoutes = require('./routes/auth-routes');
 const homeRoutes = require("./routes/home-routes");
 const adminRoutes = require("./routes/admin-routes");
-const uploadImageRoutes = require('./routes/image-route');
+const imageRoutes = require('./routes/image-route');
 
 connectToDB()
 
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/home', homeRoutes);
 app.use('/api/admin', adminRoutes);
-app.use('/api/image', uploadImageRoutes);
+app.use('/api/image', imageRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is now running on port ${PORT}`);
