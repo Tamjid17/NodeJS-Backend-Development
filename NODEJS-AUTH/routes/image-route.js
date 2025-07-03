0171ce71e7e6a4b83d9cfa2b6584cd0a8263c706
IslamTamjid <towhidulislam24@protonmail.com>
2025-07-03 22:39:18 +0600

    add deleteImageController to image route

--- NODEJS-AUTH/routes/image-route.js
+++ NODEJS-AUTH/routes/image-route.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { uploadImageController, fetchImageController} = require('../controllers/image-controller');
+const { uploadImageController, fetchImageController, deleteImageController} = require('../controllers/image-controller');
 const authMiddlware = require('../middleware/auth-middleware');
 const adminMiddleware = require("../middleware/admin-middleware");
 const uploadMiddleware = require('../middleware/upload-middleware');
 const router = express.Router();
 
 router.post("/upload", authMiddlware, adminMiddleware, uploadMiddleware.single('image'), uploadImageController);
-router.get('/all-images', authMiddlware, fetchImageController)
+router.get('/all-images', authMiddlware, fetchImageController);
+router.delete('/:id', authMiddlware, adminMiddleware, deleteImageController);
 
 module.exports = router;
