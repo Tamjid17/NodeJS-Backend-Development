5edf562e5693cd942d4b060f0c9ea603b0a8a964
IslamTamjid <towhidulislam24@protonmail.com>
2025-07-04 02:26:01 +0600

    fix: correct express.json usage and add product routes

--- MONGODB-INTERMEDIATE/server.js
+++ MONGODB-INTERMEDIATE/server.js
@@ -2,14 +2,17 @@ const dotenv = require('dotenv');
 dotenv.config()
 const express = require('express');
 const connnectToDB = require('./database/db')
+const productRoutes = require('./routes/product-routes')
 
 const app = express();
 
-app.use(express.json)
 
 const PORT = process.env.PORT || 3000;
 
 connnectToDB();
+
+app.use(express.json());
+app.use('/products', productRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is listening to port ${PORT}`)
