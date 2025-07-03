332e29ef50bd0099deaeff9f22b411972465f1e3
IslamTamjid <towhidulislam24@protonmail.com>
2025-07-03 22:37:37 +0600

    add pagination and deletion functionality for images

--- NODEJS-AUTH/controllers/image-controller.js
+++ NODEJS-AUTH/controllers/image-controller.js
@@ -1,6 +1,7 @@
 const Image = require('../models/Image');
 const uploadToCloudinary = require('../helpers/cloudinary-helper');
 const fs = require('fs');
+const cloudinary = require('../config/cloudinary');
 
 const uploadImageController = async (req, res) => {
     try {
@@ -52,7 +53,19 @@ const uploadImageController = async (req, res) => {
 
 const fetchImageController = async (req, res) => {
     try {
-        const allImages =  await Image.find({});
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 5;
+        const skip = (page - 1) * limit;
+
+        const sortBy = req.query.sortBy || 'createAt';
+        const sortOrder = req.query.sortOrder === 'asc' ? 1 : -1;
+        const totalImages = await Image.countDocuments();
+        const totalPages = Math.ceil(totalImages / limit);
+
+        const sortObj = {}
+        sortObj[sortBy] = sortOrder;
+
+        const allImages =  await Image.find().sort(sortObj).skip(skip).limit(limit);
         if(!allImages) {
             return res.status(404).json({
                 success : false,
@@ -61,6 +74,9 @@ const fetchImageController = async (req, res) => {
         }
         res.status(201).json({
             success : true,
+            currentPage : page,
+            totalPages,
+            totalImages,
             message : 'Successfully fetched all images',
             images : allImages
         })
@@ -73,4 +89,49 @@ const fetchImageController = async (req, res) => {
     }
 }
 
-module.exports = { uploadImageController, fetchImageController};
+const deleteImageController = async (req, res) => {
+    try {
+        const currentImageID = req.params.id;
+
+        //get details of the image by id from req params
+        const currentImageDetails = await Image.findById(currentImageID);
+
+        if (!currentImageDetails) {
+        return res.status(404).json({
+            success: false,
+            message: "The image you are trying to delete is not found",
+        });
+        }
+
+        const userId = req.userInfo.userId;
+
+        // check if the user who is trying to delete the image uploaded it
+        if (userId !== currentImageDetails.uploadedBy.toString()) {
+        return res.status(403).json({
+            success: false,
+
+            message: "You are not permitted to delete this image",
+        });
+        }
+
+        // first delete image from cloudinary
+        await cloudinary.uploader.destroy(currentImageDetails.publicId);
+
+        // secondly delete image from mongoDB
+        const deletedImage = await Image.findByIdAndDelete(currentImageID);
+
+        res.status(200).json({
+        success: true,
+        message: "Image deleted successfully",
+        deletedImage,
+        });
+    } catch (e) {
+    console.error("Error", e);
+    res.status(500).json({
+        success: false,
+        message: "Something went wrong, try again later",
+        });
+    }
+};
+
+module.exports = { uploadImageController, fetchImageController, deleteImageController};
