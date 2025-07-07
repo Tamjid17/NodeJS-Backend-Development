cacfbc2fce9b1a34dcb088ab0179644fee54fc2e
IslamTamjid <towhidulislam24@protonmail.com>
2025-07-08 02:09:19 +0600

    add book route to server file

--- MONGODB-INTERMEDIATE/server.js
+++ MONGODB-INTERMEDIATE/server.js
@@ -3,6 +3,7 @@ dotenv.config()
 const express = require('express');
 const connnectToDB = require('./database/db')
 const productRoutes = require('./routes/product-routes')
+const bookRoutes = require('./routes/book-routes')
 
 const app = express();
 
@@ -13,6 +14,7 @@ connnectToDB();
 
 app.use(express.json());
 app.use('/products', productRoutes);
+app.use('/reference', bookRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is listening to port ${PORT}`)
