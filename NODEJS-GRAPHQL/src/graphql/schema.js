dbc5b2003fa25a333404613f39f6990f93a3c769
IslamTamjid <towhidulislam24@protonmail.com>
2025-07-10 18:19:39 +0600

    add: define GraphQL schema for Product and related operations

---
+++ NODEJS-GRAPHQL/src/graphql/schema.js
@@ -0,0 +1,44 @@
+// defines the structure of the data
+
+const { gql } = require('graphql-tag');
+
+
+// Query types for fetching all data items or single data items
+// Mutation types for modifying/deleting or creating new data items 
+
+const typeDefs = gql`
+    type Product {
+        id: ID!
+        title: String!
+        category: String!
+        price: Float!
+        inStock: Boolean!
+    }
+
+    type Query {
+    products: [Product!]!
+    product(id: ID!): Product
+    }
+
+    type Mutation{
+
+    createProduct(
+    title: String!
+    category: String!
+    price: Float!
+    inStock: Boolean!
+    ) : Product
+
+    deleteProduct(id: ID!): Boolean
+
+    updateProduct(
+    id: ID!
+        title: String
+        category: String
+        price: Float
+        inStock: Boolean
+    ): Product
+    }
+`;
+
+module.exports = typeDefs;
