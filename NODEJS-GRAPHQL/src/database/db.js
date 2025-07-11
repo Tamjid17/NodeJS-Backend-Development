85bf94ca7b4a65ba0bdd9de41d19ec6664f6ca4b
IslamTamjid <towhidulislam24@protonmail.com>
2025-07-11 22:35:33 +0600

    add: implement database connection using mongoose and dotenv

---
+++ NODEJS-GRAPHQL/src/database/db.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+
+const connectToDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB connected successfully")
+    } catch (e) {
+        console.error("MongoDB connection failed!", e);
+        process.exit(1);
+    }
+};
+
+module.exports = connectToDB
