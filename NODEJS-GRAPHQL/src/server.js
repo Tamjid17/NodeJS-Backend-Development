fdfd36cf56cbbb2fbc5671cf15e273fd386c3702
IslamTamjid <towhidulislam24@protonmail.com>
2025-07-11 22:35:08 +0600

    refactor: update server to use environment variable for port and connect to database

--- NODEJS-GRAPHQL/src/server.js
+++ NODEJS-GRAPHQL/src/server.js
@@ -1,16 +1,19 @@
+require('dotenv').config();
 const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const connectToDB = require('./database/db');
 const typeDefs = require('./graphql/schema');
 const resolvers = require('./graphql/resolver');
 
 async function startServer(){
+    await connectToDB();
     const server = new ApolloServer({
         typeDefs,
         resolvers,
     });
 
     const { url } = await startStandaloneServer(server, {
-        listen : {port : 4000}
+        listen : {port : process.env.PORT}
     });
 
     console.log(`Server ready at: ${url}`);
